6dda3133e137a978ff529e313bf21ca906fe32f8
Joe Arrowood <JArrowood@aflac.com>
2024-02-28 06:50:52 -0500

    Added Domain Request

---
+++ Practical.AI/Agents/DomainRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Practical.AI.Agents
+{
+    // ----------------------------------------------------
+    /// <summary>
+    ///     DomainRequest Description
+    /// </summary>
+
+    public class DomainRequest
+    {
+        public int X { set; get; }
+        public int Y { set; get; }
+        public IDomain Domain { set; get; }
+        public int SenseRadius { set; get; }
+        public double[,] Terrain { set; get; }
+        public double ObstacleThreshold { set; get; }
+        public IEnumerable<Belief> InitialBeliefs { set; get; }
+    }
+}
