cceb1557d14ad53b75fd1dfe784660b27eb770d6
JoeArrow <joearrow1964@gmail.com>
2026-01-19 10:24:09 -0500

    Finished

---
+++ LinRegOnnxRunner/LinRegOnnxRunner/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace LinRegOnnxRunner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            numX = new NumericUpDown();
+            btnPredict = new Button();
+            txtY = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)numX).BeginInit();
+            SuspendLayout();
+            // 
+            // numX
+            // 
+            numX.Location = new Point(57, 40);
+            numX.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            numX.Name = "numX";
+            numX.Size = new Size(120, 23);
+            numX.TabIndex = 0;
+            // 
+            // btnPredict
+            // 
+            btnPredict.Location = new Point(80, 69);
+            btnPredict.Name = "btnPredict";
+            btnPredict.Size = new Size(75, 23);
+            btnPredict.TabIndex = 1;
+            btnPredict.Text = "Predict";
+            btnPredict.UseVisualStyleBackColor = true;
+            btnPredict.Click += btnPredict_Click;
+            // 
+            // txtY
+            // 
+            txtY.Location = new Point(67, 98);
+            txtY.Name = "txtY";
+            txtY.ReadOnly = true;
+            txtY.Size = new Size(100, 23);
+            txtY.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(227, 154);
+            Controls.Add(txtY);
+            Controls.Add(btnPredict);
+            Controls.Add(numX);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "AI Pred";
+            ((System.ComponentModel.ISupportInitialize)numX).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private NumericUpDown numX;
+        private Button btnPredict;
+        private TextBox txtY;
+    }
+}
---
+++ LinRegOnnxRunner/LinRegOnnxRunner/Form1.cs
@@ -0,0 +1,59 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
+namespace LinRegOnnxRunner
+{
+    public partial class Form1 : Form
+    {
+        private readonly InferenceSession _session;
+        private readonly string _inputName;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var modelPath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "linreg_valid_v1.onnx"
+            );
+
+            _session = new InferenceSession(modelPath);
+            _inputName = _session.InputMetadata.Keys.Single();
+        }
+
+        // ------------------------------------------------
+
+        private void btnPredict_Click(object sender, EventArgs e)
+        {
+            float x = (float)numX.Value;
+
+            var input = new DenseTensor<float>(new[] { 1, 1 });
+            input[0, 0] = x;
+
+            var inputs = new[]
+            {
+                NamedOnnxValue.CreateFromTensor(_inputName, input)
+            };
+
+            using var results = _session.Run(inputs);
+
+            float y = results
+                .First()
+                .AsTensor<float>()[0, 0];
+
+            txtY.Text = y.ToString("0");
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            _session.Dispose();
+            base.OnFormClosed(e);
+        }
+    }
+}
+
